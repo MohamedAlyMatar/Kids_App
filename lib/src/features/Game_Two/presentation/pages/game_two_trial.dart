c8610cb7974a31df60107c5c1437257dd284148c
MohamedAlyMatar <mohamedalymatar2001@gmail.com>
2024-08-28 22:59:38 +0300

    completed a beta version of the matching concept

--- lib/src/features/Game_Two/presentation/pages/game_two_trial.dart
+++ lib/src/features/Game_Two/presentation/pages/game_two_trial.dart
@@ -1,33 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:kids_app/src/core/utils/app_colors.dart';
 
-void main() {
-  runApp(MaterialApp(home: GameTwoTrial()));
+class GameTwoTrial extends StatefulWidget {
+  @override
+  State<GameTwoTrial> createState() => _GameTwoTrialState();
 }
 
-class GameTwoTrial extends StatelessWidget {
+class _GameTwoTrialState extends State<GameTwoTrial> {
+  List<ItemModel> items = [];
+  List<ItemModel> items2 = [];
+
+  late int score;
+  late bool gameOver;
+
+  @override
+  void initState() {
+    super.initState();
+    initGame();
+  }
+
+  void initGame() {
+    score = 0;
+    gameOver = false;
+
+    // Initializing the items and items2 lists
+    items = [
+      ItemModel(
+          name: "Donut",
+          icon: Icons.donut_large,
+          value: '50',
+          accepting: false),
+      ItemModel(
+          name: "Car Crash",
+          icon: Icons.car_crash,
+          value: '50',
+          accepting: false),
+      ItemModel(
+          name: "Water", icon: Icons.water, value: '50', accepting: false),
+      ItemModel(
+          name: "Water", icon: Icons.water, value: '50', accepting: false),
+      ItemModel(
+          name: "Water", icon: Icons.water, value: '50', accepting: false),
+    ];
+
+    items2 = List<ItemModel>.from(items);
+    items.shuffle();
+    items2.shuffle();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Container(
-          width: 600, // Adjust width based on your preference
-          height: 400, // Adjust height based on your preference
-          padding: EdgeInsets.all(20),
+          width: 600,
+          height: 400,
+          padding: const EdgeInsets.all(20),
           decoration: BoxDecoration(
-            color: Colors.purple.shade200, // Background color
+            color: AppColors.primaryColor,
             borderRadius: BorderRadius.circular(20),
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
-                padding: EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
                 decoration: BoxDecoration(
                   color: AppColors.primaryColor,
                   borderRadius: BorderRadius.circular(10),
                 ),
-                child: Row(
+                child: const Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
@@ -49,124 +91,130 @@ class GameTwoTrial extends StatelessWidget {
                   ],
                 ),
               ),
-              SizedBox(height: 20),
-              Expanded(
-                child: Row(
+              const SizedBox(height: 20),
+              // Choices
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Targets",
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
+                      child: Wrap(
+                        direction: Axis.horizontal,
+                        alignment: WrapAlignment.start,
+                        children: items.map((item) {
+                          return Container(
+                            decoration: BoxDecoration(
+                              color: AppColors.backgroundColor,
+                              borderRadius: BorderRadius.circular(10),
                             ),
-                          ),
-                          SizedBox(height: 10),
-                          Expanded(
-                            child: Container(
-                              color: Colors.purple.shade100,
-                              child: ListView(
-                                children: List.generate(
-                                  5,
-                                  (index) => Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Container(
-                                      height: 50,
-                                      color: Colors.white,
-                                      child: Center(
-                                          child: Text("Item ${index + 1}")),
-                                    ),
-                                  ),
-                                ),
+                            margin: const EdgeInsets.all(10),
+                            padding: const EdgeInsets.all(10),
+                            child: Draggable<ItemModel>(
+                              data: item,
+                              childWhenDragging: Icon(
+                                item.icon,
+                                color: Colors.grey,
+                              ),
+                              feedback: Icon(
+                                item.icon,
+                                color: Colors.teal,
+                                size: 50,
+                              ),
+                              child: Icon(
+                                item.icon,
+                                color: Colors.amber,
+                                size: 50,
                               ),
                             ),
-                          ),
-                          SizedBox(height: 10),
-                          ElevatedButton(
-                            onPressed: () {
-                              // Handle submit action
-                            },
-                            child: Text("Submit"),
-                            style: ElevatedButton.styleFrom(
-                              foregroundColor: Colors.black,
-                              backgroundColor: Colors.white, // Text color
-                            ),
-                          ),
-                        ],
+                          );
+                        }).toList(),
                       ),
                     ),
-                    SizedBox(width: 20),
+                    const SizedBox(width: 20),
+                    // Destinations
                     Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Destinations",
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          Expanded(
-                            child: Container(
-                              color: Colors.purple.shade100,
-                              child: ListView(
-                                children: List.generate(
-                                  5,
-                                  (index) => Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Container(
-                                      height: 50,
-                                      color: Colors.white,
-                                      child: Center(
-                                          child:
-                                              Text("Destination ${index + 1}")),
-                                    ),
-                                  ),
-                                ),
+                      child: Wrap(
+                        direction: Axis.horizontal,
+                        alignment: WrapAlignment.start,
+                        children: items2.map((item) {
+                          return DragTarget<ItemModel>(
+                            onAccept: (receivedItem) {
+                              if (item.value == receivedItem.value) {
+                                setState(() {
+                                  items.remove(receivedItem);
+                                  items2.remove(item);
+                                  score += 10;
+                                  item.accepting = false;
+                                });
+                              } else {
+                                setState(() {
+                                  score -= 5;
+                                  item.accepting = false;
+                                });
+                              }
+                            },
+                            onWillAccept: (receivedItem) {
+                              setState(() {
+                                item.accepting = true;
+                              });
+                              return true;
+                            },
+                            onLeave: (receivedItem) {
+                              setState(() {
+                                item.accepting = false;
+                              });
+                            },
+                            builder: (context, acceptedItems, rejectedItems) =>
+                                Container(
+                              decoration: BoxDecoration(
+                                color:
+                                    item.accepting ? Colors.red : Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              margin: const EdgeInsets.all(10),
+                              padding: const EdgeInsets.all(10),
+                              height: 50,
+                              width: 50,
+                              alignment: Alignment.center,
+                              child: Text(
+                                item.name,
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.amber,
+                                    fontWeight: FontWeight.bold),
                               ),
                             ),
-                          ),
-                          SizedBox(height: 10),
-                          ElevatedButton(
-                            onPressed: () {
-                              // Handle next trial action
-                            },
-                            child: Text("Next trial ->"),
-                            style: ElevatedButton.styleFrom(
-                              foregroundColor: Colors.black,
-                              backgroundColor: Colors.white,
-                            ),
-                          ),
-                        ],
+                          );
+                        }).toList(),
                       ),
                     ),
+                  ]),
+              if (gameOver) ...[
+                Column(
+                  children: [
+                    Text("Game Over. Your score is $score"),
+                    ElevatedButton(onPressed: () {}, child: Text("data"))
                   ],
-                ),
-              ),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    "ClipArt",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-              ),
+                )
+              ]
             ],
           ),
         ),
       ),
     );
   }
+}
+
+class ItemModel {
+  final String name;
+  final String value;
+  final IconData icon;
+  bool accepting;
+
+  ItemModel({
+    required this.name,
+    required this.value,
+    required this.icon,
+    this.accepting = false,
+  });
 }
